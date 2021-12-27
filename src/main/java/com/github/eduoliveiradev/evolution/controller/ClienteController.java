e728898417f69667afbedc2cb0ebc32bd7c75a5e
Eduardo Andre de Oliveira <eduquimicaufu@gmail.com>
2021-12-27 16:53:25 -0300

    validação de campos

--- src/main/java/com/github/eduoliveiradev/evolution/controller/ClienteController.java
+++ src/main/java/com/github/eduoliveiradev/evolution/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.github.eduoliveiradev.evolution.controller;
 
 import com.github.eduoliveiradev.evolution.dto.ClienteDTO;
 import com.github.eduoliveiradev.evolution.service.ClienteService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class ClienteController {
     }
 
     @PostMapping
-    public UUID criaCliente(@RequestBody ClienteDTO clienteDTO) {
+    public UUID criaCliente(@Validated @RequestBody ClienteDTO clienteDTO) {
 
         return clienteService.save(clienteDTO);
     }
