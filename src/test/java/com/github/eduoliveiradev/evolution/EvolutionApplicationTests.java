cabc38193e4a66ea49a1013e03315ca7bd65fc12
Eduardo Andre de Oliveira <eduquimicaufu@gmail.com>
2021-12-27 15:11:13 -0300

    spring start

---
+++ src/test/java/com/github/eduoliveiradev/evolution/EvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eduoliveiradev.evolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
